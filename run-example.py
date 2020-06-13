2f6eb609131673e82987cbdedd0174dd70995f59
Erik Driessen <erik.driessen@outlook.com>
2020-06-13 14:47:43 +0200

    Update run-example.py

--- run-example.py
+++ run-example.py
@@ -3,7 +3,7 @@ from analyse import analyse_files_and_store_in_csv
 import pandas as pd
 
 # analyse_files_and_store_in_csv('songs/avicii tim', 'avicii tim')
-df = pd.read_csv('output/avicii tim csv.csv').sort_values(by='index').reset_index()
+df = pd.read_csv('output/avicii tim.csv').sort_values(by='index').reset_index()
 
 scatter_plot_from_dataframe(
     dataframe=df,
@@ -17,7 +17,7 @@ plot_path_from_dataframe(
   dataframe=df,
   show_dots=True,
   disable_grids=False,
-  line_type='s',
+  line_type='c',
   line_color='black',
   emogrid=True,
 )
